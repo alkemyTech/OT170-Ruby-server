652902d208389dcc4fe9e097b1d9cdd5e36ce63a
Christians Cartaya <christianscartaya@gmail.com>
2022-03-27 19:47:37 -0300

    feat OT-170-32-: endpoint delete user

--- db/schema.rb
+++ db/schema.rb
@@ -95,7 +95,7 @@ ActiveRecord::Schema.define(version: 2022_03_30_041826) do
 
   create_table "sessions", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.datetime "last_used_at", default: "2022-04-03 22:17:02"
+    t.datetime "last_used_at", default: "2022-04-03 22:28:59"
     t.boolean "status", default: true
     t.string "token"
     t.datetime "created_at", precision: 6, null: false
