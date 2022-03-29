7b372b9023216e93af8970647b81bfb02dad3e34
pablo ayala <pabloagustinayalaalvarez@gmail.com>
2022-03-29 10:55:35 -0300

    feat-OT170-16-create-migration-controller-rol

---
+++ db/migrate/20220328195127_create_rols.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateRols < ActiveRecord::Migration[6.1]
+  def change
+    create_table :rols do |t|
+      t.string :name, null: false
+      t.string :description, null: false
+
+      t.timestamps
+    end
+  end
+end
