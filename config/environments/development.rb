3e2bb406838ebe80f5afa37144d343cfa33f352b
Fernando Gonzalez <fernandogonzalezgaete@gmail.com>
2022-03-30 20:03:54 -0300

    feat OT-170-27: add mailcather setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,11 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # Setup for mailcatcher
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  config.action_mailer.raise_delivery_errors = false
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
