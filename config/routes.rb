c23b196a03f0a1ad001af20dbb96d218f3649657
gisela valeria <giselavaleria19@gmail.com>
2022-04-03 20:35:14 -0300

    feat-OT170-37-endpoint-for-updating-public-organization-data

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
+        post '/organizations/public', to: 'organizations#create'
       namespace :auth do
         post '/register', to: 'registrations#create'
         post '/login', to: 'sessions#create'
