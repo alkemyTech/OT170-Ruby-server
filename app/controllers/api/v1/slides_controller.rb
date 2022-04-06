09f4bd156cc4de223f1130ad9bc5900ae59cf54b
fabian zacarola <fabianzacarola@gmail.com>
2022-04-05 23:30:33 -0300

    feat-0T170-59-endpoint-update-slides

--- app/controllers/api/v1/slides_controller.rb
+++ app/controllers/api/v1/slides_controller.rb
@@ -3,10 +3,30 @@
 module Api
   module V1
     class SlidesController < ApplicationController
+      before_action :set_slide, only: %i[update]
+
       def index
         @organization = Organization.find(params[:organization_id])
         @slides = @organization.slides.all
         render json: SlideSerializer.new(@slides).serializable_hash
+      end
+
+      def update
+        if @slide.update(slide_params)
+          render json: SlideSerializer.new(@slide).serializable_hash
+        else
+          render json: @slide.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def set_slide
+        @slide = Slide.find(params[:id])
+      end
+
+      def slide_params
+        params.require(:slide).permit(:image, :text, :order, :organization_id)
       end
     end
   end
