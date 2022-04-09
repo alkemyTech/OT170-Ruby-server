bccdb4e5bb15347a7607eb2bf2ccf8ed00ec406d
gisela valeria <giselavaleria19@gmail.com>
2022-04-08 21:53:16 -0300

    feat/OT170-62: end point list contacts

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
 
       resources :users, only: %i[index destroy]
 
-      resources :contacts, only: %i[create]
+      resources :contacts, only: %i[index create]
     end
   end
 end
