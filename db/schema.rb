e4571a80b8315a345e70f2b9b984a4709ddf9a1d
Christians Cartaya <christianscartaya@gmail.com>
2022-04-22 09:50:07 -0300

    fix/OT170-75-crear-seed-usuarios

--- db/schema.rb
+++ db/schema.rb
@@ -131,7 +131,7 @@ ActiveRecord::Schema.define(version: 2022_04_15_223157) do
 
   create_table "sessions", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.datetime "last_used_at", default: "2022-04-21 20:48:37"
+    t.datetime "last_used_at", default: "2022-04-22 12:47:19"
     t.boolean "status", default: true
     t.string "token"
     t.datetime "created_at", precision: 6, null: false
