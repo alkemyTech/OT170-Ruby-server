369165cd3786408f1c55c3e4c7ea7d63c9dab1f1
Fernando Gonzalez <fernandogonzalezgaete@gmail.com>
2022-04-22 12:33:47 -0400

    feat OT-170-27: fix controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,16 +6,13 @@ module Api
       # POST /users or /users.json
       def create
         @user = User.new(user_params)
-
         respond_to do |format|
           if @user.save
             # Tell the UserMailer to send a welcome email after save
             UserMailer.with(user: @user).welcome_email.deliver_later
 
-            format.html { redirect_to(@user, notice: 'User was successfully created.') }
             format.json { render json: @user, status: :created, location: @user }
           else
-            format.html { render action: 'new' }
             format.json { render json: @user.errors, status: :unprocessable_entity }
           end
         end
